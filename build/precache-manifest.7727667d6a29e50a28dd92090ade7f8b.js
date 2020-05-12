6d3388025aa4a385cb03c20464eb4d7237c6469e
Pichaiporn <51431293+Pichaiporn@users.noreply.github.com>
2020-05-13 00:02:13 +0700

    Add files via upload

---
+++ build/precache-manifest.7727667d6a29e50a28dd92090ade7f8b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "82cfe0c87f9b3fc330074845d43aee80",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2d6a202ecb07ea22102b",
+    "url": "/static/css/main.c035480a.chunk.css"
+  },
+  {
+    "revision": "1500ae43b738076e6ede",
+    "url": "/static/js/2.5dda02e7.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.5dda02e7.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2d6a202ecb07ea22102b",
+    "url": "/static/js/main.d720ea7c.chunk.js"
+  },
+  {
+    "revision": "b04f7287872513f8ed7b",
+    "url": "/static/js/runtime-main.53f5b932.js"
+  },
+  {
+    "revision": "c97cb84b74a52edcb496295687e87822",
+    "url": "/static/media/cold-bg.c97cb84b.jpg"
+  },
+  {
+    "revision": "635ed87d2cc826fb213057ac40dfaff1",
+    "url": "/static/media/warm-bg.635ed87d.jpg"
+  }
+]);
